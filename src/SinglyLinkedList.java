10d26c4dc86de7a83e0883675a0a3f285008c29b
Abhisek <mandalabhisek20@gmail.com>
2025-05-20 17:43:48 +0530

    Insert method added

--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -10,4 +10,20 @@ class Node{
 
 
 public class SinglyLinkedList {
+    Node head;
+
+    //add node at end
+    public void insert(int data){
+        Node node =  new Node(data);
+        if(head==null){
+            head=node;
+        }else{
+            Node temp = head;
+            while (temp.next != null){
+               temp = temp.next;
+            }
+            temp.next= node;
+        }
+    }
+
 }
